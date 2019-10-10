9d0a04bd1b8723b26747ab5eada1963d2200f3b5
counter2015 <voidcounter@gmail.com>
2019-10-11 03:51:08 +0800

    [fix] typesafe config depend.

--- src/main/g8/build.sbt
+++ src/main/g8/build.sbt
@@ -12,7 +12,8 @@ lazy val root = (project in file(".")).
     libraryDependencies ++= Seq(
       logback,
       scalaLogging,
-      scalaTest
+      scalaTest,
+      typesafeConfig
     )
   )
 
--- src/main/g8/project/Dependencies.scala
+++ src/main/g8/project/Dependencies.scala
@@ -5,9 +5,12 @@ object Dependencies {
   val logbackVersion                    =             "1.2.3"
   val scalaLoggingVersion               =             "3.9.2"
   val scalaTestVesrion                  =             "3.0.8"
+  val typesafeConfigVersion             =             "1.3.4"
 
   // Libraries
   val logback              = "ch.qos.logback"             %  "logback-classic"         % logbackVersion
   val scalaLogging         = "com.typesafe.scala-logging" %% "scala-logging"           % scalaLoggingVersion
   val scalaTest            = "org.scalatest"              %% "scalatest"               % scalaTestVesrion         % Test
+  val typesafeConfig       = "com.typesafe"               % "config"                   % typesafeConfigVersion
+
 }
