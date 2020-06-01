0adb75653bdafe936b8cdccb0d2d3642a0dfd966
counter2015 <voidcounter@gmail.com>
2020-06-01 16:09:13 +0800

    apply deprecated changes since scalaTest 3.1.0

--- src/main/g8/src/test/scala/HelloSpec.scala
+++ src/main/g8/src/test/scala/HelloSpec.scala
@@ -1,6 +1,7 @@
-import org.scalatest.{FlatSpec, Matchers}
+import org.scalatest.funsuite.AnyFlatSpec
+import org.scalatest.matchers.should.Matchers
 
-class HelloSpec extends FlatSpec with Matchers {
+class HelloSpec extends AnyFlatSpec with Matchers {
   "The Hello object" should "say hello" in {
     Hello.greeting shouldEqual "hello"
   }
